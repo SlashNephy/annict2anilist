2d04f72fe408b70f35110573274ed2f50300773c
SlashNephy <spica@starry.blue>
2022-10-11 23:19:48 +0900

    arm で相互に作品を引けるように

--- arm.go
+++ arm.go
@@ -4,19 +4,25 @@ import (
 	"context"
 	"io"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/goccy/go-json"
+	"golang.org/x/exp/slices"
 )
+
+type ArmDatabase struct {
+	Entries []ArmEntry
+}
 
 type ArmEntry struct {
 	MalID       int `json:"mal_id"`
 	AniListID   int `json:"anilist_id"`
 	AnnictID    int `json:"annict_id"`
-	SyoboCalTID int `json:"syobocal_tid"`
+	SyobocalTID int `json:"syobocal_tid"`
 }
 
-func FetchArmDatabase(ctx context.Context) ([]ArmEntry, error) {
+func FetchArmDatabase(ctx context.Context) (*ArmDatabase, error) {
 	client := &http.Client{
 		Timeout: 10 * time.Second,
 	}
@@ -44,5 +50,100 @@ func FetchArmDatabase(ctx context.Context) ([]ArmEntry, error) {
 		return nil, err
 	}
 
-	return entries, nil
+	return &ArmDatabase{
+		Entries: entries,
+	}, nil
+}
+
+func (d *ArmDatabase) FindByAnnictID(id int) (*ArmEntry, bool) {
+	if id == 0 {
+		return nil, false
+	}
+
+	index := slices.IndexFunc(d.Entries, func(entry ArmEntry) bool {
+		return entry.AnnictID != 0 && entry.AnnictID == id
+	})
+	if index < 0 {
+		return nil, false
+	}
+
+	return &d.Entries[index], true
+}
+
+func (d *ArmDatabase) FindByAniListID(id int) (*ArmEntry, bool) {
+	if id == 0 {
+		return nil, false
+	}
+
+	index := slices.IndexFunc(d.Entries, func(entry ArmEntry) bool {
+		return entry.AniListID != 0 && entry.AniListID == id
+	})
+	if index < 0 {
+		return nil, false
+	}
+
+	return &d.Entries[index], true
+}
+
+func (d *ArmDatabase) FindByMalID(id int) (*ArmEntry, bool) {
+	if id == 0 {
+		return nil, false
+	}
+
+	index := slices.IndexFunc(d.Entries, func(entry ArmEntry) bool {
+		return entry.MalID != 0 && entry.MalID == id
+	})
+	if index < 0 {
+		return nil, false
+	}
+
+	return &d.Entries[index], true
+}
+
+func (d *ArmDatabase) FindBySyobocalTID(tid int) (*ArmEntry, bool) {
+	if tid == 0 {
+		return nil, false
+	}
+
+	index := slices.IndexFunc(d.Entries, func(entry ArmEntry) bool {
+		return entry.SyobocalTID != 0 && entry.SyobocalTID == tid
+	})
+	if index < 0 {
+		return nil, false
+	}
+
+	return &d.Entries[index], true
+}
+
+func (d *ArmDatabase) FindForAniList(annictID int, malID string, syobocalID int) (*ArmEntry, bool) {
+	// 1. Annict ID から探す
+	arm, found := d.FindByAnnictID(annictID)
+	if found {
+		return arm, found
+	}
+
+	// 2. MAL ID から探す
+	if malID != "" {
+		malIntID, err := strconv.Atoi(malID)
+		if err == nil {
+			arm, found = d.FindByMalID(malIntID)
+			if found {
+				return arm, found
+			}
+		}
+	}
+
+	// 3. しょぼいカレンダー TID から探す
+	return d.FindBySyobocalTID(syobocalID)
+}
+
+func (d *ArmDatabase) FindForAnnict(aniListID, malID int) (*ArmEntry, bool) {
+	// 1. AniList ID から探す
+	arm, found := d.FindByAniListID(aniListID)
+	if found {
+		return arm, found
+	}
+
+	// 2. MAL ID から探す
+	return d.FindByMalID(malID)
 }
